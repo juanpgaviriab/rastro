f2dedde0a4cd5ebdb39550eebb541b21b35ec305
juanpgaviriab <64409923+juanpgaviriab@users.noreply.github.com>
2023-04-07 18:01:36 +0200

    Update sketch_archivo.js

--- sketch/sketch_archivo.js
+++ sketch/sketch_archivo.js
@@ -1,3 +1,5 @@
+import data from '/contenido/contenido.json' assert { type: 'json' };
+
 const seccionIndex = document.querySelectorAll('.seccion').length;
 
 let titulo = [];
@@ -12,18 +14,11 @@ for (let index = 0; index < seccionIndex; index++) {
     titulo[index] = document.getElementById("titulo" + index);
     artista[index] = document.getElementById("artista" + index);
     descripcion[index] = document.getElementById("descripcion" + index);
-    fetch('/contenido/contenido.json')
-    .then(response => response.json())
-    .then(data => {
-        titulo[index].innerHTML = data[index].titulo;
-        artista[index].innerHTML = data[index].artistas;
-        descripcion[index].innerHTML = data[index].texto;
-    })
-    .catch(error => console.error(error));
-    
+
+    titulo[index].innerHTML = data[index].titulo;
+    artista[index].innerHTML = data[index].artistas;
+    descripcion[index].innerHTML = data[index].texto;
 }
-
-
 
 
 

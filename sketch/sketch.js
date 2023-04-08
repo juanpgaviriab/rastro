794df66a0d5262b4feded30c9b79be71c6bd503a
juanpgaviriab <64409923+juanpgaviriab@users.noreply.github.com>
2023-04-08 10:00:12 +0200

    Update sketch.js

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,4 +1,4 @@
-import data from '/contenido/contenido.json' assert { type: 'json' };
+import data from './contenido/contenido.json' assert { type: 'json' };
 
 const boton = document.getElementById("b" + 1);
 const botonIndex = document.querySelectorAll('.boton').length;
